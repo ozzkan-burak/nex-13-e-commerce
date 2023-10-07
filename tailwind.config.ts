5bf4e51b329ad38385348dbd4e54827e6fabbaef
ozzkan-burak <brk.ozzkan@gmail.com>
2023-10-07 13:13:49 +0300

    episode1

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,3 +1,4 @@
+import daisyui from 'daisyui'
 import type { Config } from 'tailwindcss'
 
 const config: Config = {
@@ -15,6 +16,26 @@ const config: Config = {
       },
     },
   },
-  plugins: [],
+  plugins: [require('daisyui')],
+  daisyui: {
+    themes: [
+      {
+        lightTheme: {
+          primary: "#f4aa3a",
+          secondary: "#f4f4a1",
+          accent: "#1be885",
+          neutral: "#272136",
+          "base-100": "#ffffff",
+          info: "#778ad4",
+          success: "#23b893",
+          warning: "#f79926",
+          error: "#ea535a",
+          body: {
+            "background-color": "#e3e6e6",
+          },
+        },
+      },
+    ]
+  }
 }
 export default config
