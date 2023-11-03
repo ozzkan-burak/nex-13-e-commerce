7dae9966b7b4b46b4e5394496bacac2d9785667f
ozzkan-burak <brk.ozzkan@gmail.com>
2023-11-03 22:22:47 +0300

    episode5

--- src/app/add-product/page.tsx
+++ src/app/add-product/page.tsx
@@ -1,36 +1,56 @@
+import { prisma } from "@/lib/db/prisma";
+
+export const metadata = {
+  title: "Add Product - Ecommerce",
+};
+
+async function addProduct(formData: FormData) {
+  "use server";
+  const name = formData.get("name")?.toString();
+  const description = formData.get("description")?.toString();
+  const imageUrl = formData.get("imageUrl")?.toString();
+  const price = Number(formData.get("price" || 0));
+
+  await prisma.product.create({
+    data: {},
+  });
+}
+
 const AddProduct = () => {
   return (
     <div>
       <h1 className="text-lg mb-3 font-bold">Add Product Page</h1>
-      <input
-        required
-        name="name"
-        placeholder="Name"
-        className="input-bordered input mb-2 w-full"
-      />
-      <textarea
-        required
-        name="description"
-        placeholder="Description"
-        className="textarea-bordered textarea mb-2 w-full"
-      ></textarea>
-      <input
-        required
-        name="imageUrl"
-        placeholder="Image Url"
-        type="url"
-        className="input-bordered input mb-2 w-full"
-      />
-      <input
-        required
-        name="price"
-        placeholder="Price"
-        type="number"
-        className="input-bordered input mb-2 w-full"
-      />
-      <button type="submit" className="btn btn-primary btn-block">
-        Add Product
-      </button>
+      <form action={addProduct}>
+        <input
+          required
+          name="name"
+          placeholder="Name"
+          className="input-bordered input mb-2 w-full"
+        />
+        <textarea
+          required
+          name="description"
+          placeholder="Description"
+          className="textarea-bordered textarea mb-2 w-full"
+        ></textarea>
+        <input
+          required
+          name="imageUrl"
+          placeholder="Image Url"
+          type="url"
+          className="input-bordered input mb-2 w-full"
+        />
+        <input
+          required
+          name="price"
+          placeholder="Price"
+          type="number"
+          className="input-bordered input mb-2 w-full"
+        />
+        <button type="submit" className="btn btn-primary btn-block">
+          Add Product
+        </button>
+      </form>
     </div>
   );
 };
--- src/lib/db/prisma.ts
+++ src/lib/db/prisma.ts
@@ -1,10 +1,9 @@
 import { PrismaClient } from "@prisma/client";
 
+const globalForPrisma: { prisma: any } = globalThis as unknown as {
+  prisma: PrismaClient | undefined;
+};
 
-const globalForPrisma: { prisma: any; } = globalThis as unknown as {
-  prisma: any = PrismaClient | undefined;
-}
+export let prisma = globalForPrisma.prisma ?? new PrismaClient();
 
-export let prisma = globalForPrisma.prisma ?? new PrismaClient: any();
-
-if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma;
+if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
